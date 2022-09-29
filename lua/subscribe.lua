a1f07c5c32d85ab08ec544d6050091f400fc1983
Bobby Iliev <bobby@bobbyiliev.com>
2022-09-29 19:01:21 +0300

    Change tail to subscribe in all examples

--- lua/subscribe.lua
+++ lua/subscribe.lua
@@ -4,7 +4,7 @@ local env = assert (driver.postgres())
 local con = assert(env:connect("postgresql://MATERIALIZE_USERNAME:MATERIALIZE_PASSWORD@MATERIALIZE_HOST:6875/materialize?sslmode=enabled"))
 con:setautocommit(false)
 
-assert (con:execute("DECLARE c CURSOR FOR TAIL my_view"))
+assert (con:execute("DECLARE c CURSOR FOR SUBSCRIBE my_view"))
 
 while(true) do
     for timestamp, diff, symbol, avg in utils.rows(con,"FETCH ALL c") do
