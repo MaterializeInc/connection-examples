2748d61bd4d8d96189213b716a8fa4995a7abd36
Marta Paes <marta.paes.moreira@gmail.com>
2022-11-07 09:37:57 +0100

    Update module name in subscribe-psycopg3.py

--- python/subscribe-psycopg3.py
+++ python/subscribe-psycopg3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import psycopg3
+import psycopg
 import sys
 
 dsn = "user=MATERIALIZE_USERNAME password=MATERIALIZE_PASSWORD host=MATERIALIZE_HOST port=6875 dbname=materialize sslmode=require"
-conn = psycopg3.connect(dsn)
+conn = psycopg.connect(dsn)
 
 with conn.cursor() as cur:
     for row in cur.stream("SUBSCRIBE t"):
